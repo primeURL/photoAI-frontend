3c3ac341164ed836ece957e37f16298ef1a5976f
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2025-03-25 21:36:23 +0530

    Fixed

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -3,13 +3,13 @@ import { createPagesFunctionHandler } from "@react-router/cloudflare";
 import * as build from "../build/server/index"
 
 export const onRequest = createPagesFunctionHandler({
-  build
-//   mode: process.env.NODE_ENV,
-//   getLoadContext: (request, context) => {
-//     return {
-//       env: context.env,
-//       request,
-//       context,
-//     };
-//   },
+  build,
+  mode: process.env.NODE_ENV,
+  getLoadContext: (request, context) => {
+    return {
+      env: context.env,
+      request,
+      context,
+    };
+  },
 });
